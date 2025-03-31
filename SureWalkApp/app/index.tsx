ea6ffac6089e8cb2a7a2a5bdbca65f78ecd1c9b4
Sean Sy <sy.seanimmanuel@gmail.com>
2025-03-30 20:24:35 -0500

    chore: setup project structure with expo, nativewind, tailwind
    - Set up Expo project with basic configuration - Installed Tailwind CSS and NativeWind for styling - Configured Babel for Tailwind support in React Native - Added initial files and structure for app components - Set up safe area context for responsive layout

---
+++ SureWalkApp/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-5xl text-ut-burntorange font-bold">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
