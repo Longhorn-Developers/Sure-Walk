b2fb109fd37ebb24ee4c1de32cdb2d55e05d2399
Sean Sy <sy.seanimmanuel@gmail.com>
2025-04-16 15:11:40 -0500

    feat(app): add home layout and tabs with basic routing

---
+++ SureWalkApp/app/(tabs)/order.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+const order = () => {
+  return (
+    <SafeAreaView>
+      <Text>Order</Text>
+    </SafeAreaView>
+  );
+};
+
+export default order;
---
+++ SureWalkApp/app/(tabs)/settings.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+const settings = () => {
+  return (
+    <SafeAreaView>
+      <Text>Settings</Text>
+    </SafeAreaView>
+  );
+};
+
+export default settings;
