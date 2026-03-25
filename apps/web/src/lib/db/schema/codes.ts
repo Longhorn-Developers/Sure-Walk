b71e2f4487efc1f72783f613c04a0582f62e29b6
Pranav Pakanati <pranav.pakanati@gmail.com>
2026-03-25 17:38:37 -0500

    feat(web): create schema for signup / login features

---
+++ apps/web/src/lib/db/schema/codes.ts
@@ -0,0 +1,23 @@
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { accounts } from "./accounts";
+import { randomInt, randomUUID } from "crypto";
+import { sql } from "drizzle-orm";
+
+export const codes = sqliteTable("codes", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => randomUUID()),
+  accountID: text("account_id")
+    .references(() => accounts.id)
+    .notNull(),
+  identifier: text("identifier").notNull(),
+  newFirstName: text("first_name"),
+  newLastName: text("last_name"),
+  code: text("code", { length: 6 })
+    .$defaultFn(() => randomInt(100000, 999999).toString())
+    .unique()
+    .notNull(),
+  expiresAt: text("expires_at")
+    .default(sql`(DATETIME('now', '+10 minutes'))`)
+    .notNull(),
+});
